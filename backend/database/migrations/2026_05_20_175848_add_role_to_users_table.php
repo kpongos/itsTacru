d6c4963880701669a3a4f3abad6653f3eafa65df
Kryzsha R. Pongos <pongos.kryzsha@gmail.com>
2026-05-21 02:09:11 +0800

    Roles of users updated

---
+++ backend/database/migrations/2026_05_20_175848_add_role_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::table('users', function (Blueprint $table) {
+        // We'll store roles as a string: 'volunteer', 'admin', 'supervisor'
+        $table->string('role')->default('volunteer');
+    });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
